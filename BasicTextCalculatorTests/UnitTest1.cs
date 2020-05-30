892f9ffedc24ec240f42cc3c030c6be22355e31e
Christopher Rizzo <christopherrizzo@spscc.edu>
2020-05-29 22:39:36 -0700

    Tests added

---
+++ BasicTextCalculatorTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+using BasicTextCalculator;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void RealityTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void CantDivideByZero()
+        {
+            var c = new Calculator();
+            try
+            {
+                c.DivideNumbers(1, 0);
+                Assert.Fail();
+            }
+            catch(System.DivideByZeroException)
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void OnePlusOneIsTwo()
+        {
+            var c = new Calculator();
+
+            var result = c.AddNumbers(1, 1);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
